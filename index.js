49162651a4d078e1f29cc674904f9d343f0b0932
Shahryar Shahid <SShahid@harriscomputer.com>
2021-05-24 01:57:15 +0500

    make it better

--- index.js
+++ index.js
@@ -183,7 +183,7 @@ app.get('*', function(req, res) {
 //     res.send("hello");
 // });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 //const port = 3002;
 app.listen(port, () => {
   //SS-CHANGED
